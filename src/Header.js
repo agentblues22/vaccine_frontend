14de4af2167883a27e99d51b51aa34370496e648
agentblues22 <georgyroy007@gmail.com>
2021-05-15 18:12:14 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,77 @@
+import react, {Component} from "react";
+import axios from "axios";
+import "./form.css"
+import Contentr from "./contents";
+
+
+class Header extends Component{
+  constructor(props){
+    super(props)
+
+    this.state={
+      district: "Select District",
+      vaccine:""
+    }
+  }
+  handleDistrictChange=(event)=>{
+    this.setState({
+      district:event.target.value
+    })
+
+  }
+  handleSubmit=(event)=>{
+    
+    axios.get("/vaccine",{ params:{distr : this.state.district}}).then(response =>{
+      
+      this.setState({
+       vaccine: response.data.availability
+      });
+      
+    });
+
+    event.preventDefault()
+
+  }
+
+  render(){
+    return(
+      <div classname="mainfile">
+      <form onSubmit={this.handleSubmit}>
+        <div class="container1">
+          
+          <label>District</label>
+          <select class="select" value={this.state.district} onChange={this.handleDistrictChange}>
+          <option value="000">select District</option>
+          <option value="301">Alappuzha</option>
+          <option value="307">Eranakulam</option>
+          <option value="306">Idukki</option>
+          <option value="297">Kannur</option>
+          <option value="295">Kasargod</option>
+          <option value="298">Kollam</option>
+          <option value="304">Kottayam</option>
+          <option value="305">Kozhikode</option>
+          <option value="302">Malappuram</option>
+          <option value="308">Palakkad</option>
+          <option value="300">Pathanamthitta</option>
+          <option value="296">Thiruvananthapuram</option>
+          <option value="303">Thrissur</option>
+          <option value="299">Wayanad</option>
+          
+          </select>
+          <button class="button" type="submit">Check</button>
+          
+        </div>
+        <h1> </h1>
+        <div>
+          <Contentr vaccine={this.state.vaccine}/>
+        </div>
+        
+        
+        
+      </form>
+      </div>
+      
+    )
+  }
+}
+export default Header
---
+++ src/contents.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./form.css"
+
+class Contentr extends React.Component{
+   
+   
+  
+    render(){
+
+      
+
+      if(this.props.vaccine==null){
+
+        return(
+          <div class="container1">No Centers Available</div>
+        )
+      }
+      else{
+
+       var k= Array.from(this.props.vaccine)
+
+    
+      return(
+       
+           k.map((rawdata,i)=>
+          
+             <div class="container1">
+             <h1>{rawdata.name}</h1>
+             <ul>
+               <li>center adress:{" "} {rawdata.address}</li>
+               <li>pincode:{" "} {rawdata.pincode}</li>
+               <li>from:{rawdata.from}{" "} to:{rawdata.to}</li>
+               <li>fee type:{" "}{rawdata.fee_type}</li>
+               <h2>available capacity: {rawdata.sessions[0].available_capacity}</h2>
+             </ul>
+             </div>
+           
+           )
+     
+      )
+      } 
+    }
+}
+  
+  export default Contentr
